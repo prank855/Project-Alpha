f5e935f54c1e91889f4f173873741149c5f94a94
prank855 <prank855@gmail.com>
2021-05-10 01:30:52 -0700

    improve build file size optimization

---
+++ src/readme.md
@@ -0,0 +1,5 @@
+dev: npm run dev:client, npm run dev:server
+
+to build: npm run build
+start server build: npm run start:server
+start clinet: open ./dist/index.html
--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 dotenv.config();
 const { NODE_ENV, PORT } = process.env;
+const isProd = process.env.NODE_ENV === 'production';
 
 const development = NODE_ENV !== 'production';
 
@@ -15,49 +17,59 @@ const development = NODE_ENV !== 'production';
  * @type {import('webpack').Configuration}
  */
 module.exports = {
-  mode: development ? 'development' : 'production',
-  entry: {
-    app: ['@babel/polyfill', './src/client/index'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(NODE_ENV),
-        PORT: JSON.stringify(PORT),
-      },
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: './src/client/index.html',
-      filename: './index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  devtool: 'inline-source-map',
-  // eslint-disable-next-line unicorn/prevent-abbreviations
-  devServer: {
-    contentBase: './dist',
-    port: 3000,
-    historyApiFallback: true,
-    overlay: true,
-  },
-  optimization: {
-    minimizer: [new TerserWebpackPlugin()],
-    noEmitOnErrors: true,
-  },
+	mode: development ? 'development' : 'production',
+	entry: {
+		app: ['@babel/polyfill', './src/client/index']
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify(NODE_ENV),
+				PORT: JSON.stringify(PORT)
+			}
+		}),
+		new CleanWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: './src/client/index.html',
+			filename: './index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loaders: ['babel-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	devtool: isProd ? '' : 'inline-source-map',
+	// eslint-disable-next-line unicorn/prevent-abbreviations
+	devServer: {
+		contentBase: './dist',
+		port: 3000,
+		historyApiFallback: true,
+		overlay: true
+	},
+	optimization: {
+		minimizer: [new TerserWebpackPlugin()],
+		noEmitOnErrors: true,
+		moduleIds: 'size',
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			}
+		}
+	}
 };
